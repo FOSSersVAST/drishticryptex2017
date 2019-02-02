1cd8e766b0bb3d7cede772dc3b98e8b6a35d0b16
Subin Siby <subins2000@gmail.com>
2019-02-02 21:44:30 +0530

    add 9,10

--- application/views/levels/level_9.php
+++ application/views/levels/level_9.php
@@ -1,11 +1,9 @@
-<style>
-	body{background-color:black;}
-</style>
 <div class="container">
-  <div class="row">
+<div class="row">
 <div class="col-md-12 wrapper">
-<font size="5" face="Times New Roman", Georgia, Serif" color="white">kxWZpZmcpVXbgQXYgwWZ2VGbgQHel5GIyV3b5BycphGdgcmbpt2YhJ3YgI2bqBCZv92Z</font>
-<!--# 46 -->
+<center>
+<img src = "<?php echo base_url();?>assets/images/L9cdske2.jpg" width="600" />
+</center>
 </div>
 </div>
 </div>

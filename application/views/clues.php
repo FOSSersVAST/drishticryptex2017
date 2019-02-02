c035cd2d0ba04339f624c042368a5aae2224055e
Subin Siby <subins2000@gmail.com>
2019-02-02 21:36:00 +0530

    Add 7,8 levels

--- application/views/clues.php
+++ application/views/clues.php
@@ -4,7 +4,7 @@
   <p>Aww.. you got stuck on a level ?</p>
   <div class="row">
     <div class="col-md-12 wrapper">
-      Extra clues can be found in our Instagram, Telegram channel or Twitter :
+      Extra clues can be found in our <a href="https://instagram.com/fossersvast" style="color: #fff;text-decoration: underline;">Instagram</a>, Telegram channel or Twitter :
 
       <a href="http://fossers.vidyaacademy.ac.in/contact" class="btn btn-default">See our social media links</a>
     </div>

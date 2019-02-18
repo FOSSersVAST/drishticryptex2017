3f252842b3165e6be35e406111111b4709c3c9ed
Subin Siby <subins2000@gmail.com>
2019-02-18 17:17:51 +0530

    End of game msg

--- application/views/endofgame.php
+++ application/views/endofgame.php
@@ -2,7 +2,13 @@
 <div class="container">
 <div class="row">
 <div class="col-md-12 wrapper">
-The game has officially reached an end at 19 March 2017 00:00 IST. Thanks for participating. Check the leaderboard to know the winners. See you next time.
+The game has officially reached an end. Thanks for playing ! Hope you have learnt new things through this journey !<br/> Check the leaderboard to know the winners. See you next time :)
+<br/><br/>
+<a href="http://fossers.vidyaacademy.ac.in/vyvidh19/">
+  <img src="http://fossers.vidyaacademy.ac.in/img/vyvidh-2019/cover.jpg" width="100%" />
+  <br/><br/>
+  <p style="background: #fff;color: #000;padding: 10px;">And We invite you to <a href="http://vyvidh.vidyaacademy.ac.in/">Vyvidh'19</a> on February 8th & 9th.</p>
+</a>
 </div>
 </div>
 </div>
